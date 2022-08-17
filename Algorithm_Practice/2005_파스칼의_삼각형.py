0fcd7848e55c1b61858a67d9d3af9680b8ee9e74
dokkyunYU <ydgar001@naver.com>
2022-08-17 18:20:19 +0900

    0817 TIL

---
+++ Algorithm_Practice/2005_파스칼의_삼각형.py
@@ -0,0 +1,22 @@
+# 2005. 파스칼의 삼각형
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5P0-h6Ak4DFAUq
+
+
+def pascal_triangle(number):
+    pasacal_list = [[1] for _ in range(11)]
+    pasacal_list[2] += [1]
+    if number > 2:
+        for i in range(3, number + 1):
+            for j in range(len(pasacal_list[i - 1]) - 1):
+                pasacal_list[i].append(pasacal_list[i - 1][j] + pasacal_list[i - 1][j + 1])
+            pasacal_list[i].append(1)
+    for k in range(1, number + 1):
+        print(*pasacal_list[k])
+
+
+T = int(input())
+
+for test_count in range(1, T + 1):
+    N = int(input())
+    print(f"#{test_count}")
+    pascal_triangle(N)
