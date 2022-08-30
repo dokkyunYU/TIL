581d2686f0f5286c0e25f87726c93926135feca2
dokkyunYU <ydgar001@naver.com>
2022-08-30 17:11:07 +0900

    0830 Algorithm

--- Algorithm_Practice/self_practice/1983_조교의_성적_매기기/1983_조교의_성적_매기기_1.py
+++ Algorithm_Practice/self_practice/1983_조교의_성적_매기기/1983_조교의_성적_매기기_1.py
@@ -3,7 +3,10 @@
 
 import sys
 
+# from filecmp import cmp
+
 sys.stdin = open("input.txt")
+# sys.stdout = open("output2.txt", "w")
 
 for test_case in range(1, int(input()) + 1):
     student_amount, what_grade = map(int, input().split())
@@ -11,4 +14,10 @@ for test_case in range(1, int(input()) + 1):
     for _ in range(student_amount):
         i, j, k = map(int, input().split())
         all_scores.append(i * (7 / 20) + j * (9 / 20) + k * (4 / 20))
-    print(all_scores)
+    want_to_know = all_scores[what_grade - 1]
+    all_scores.sort()
+    the_grade = all_scores.index(want_to_know) // (student_amount // 10)
+    grade_list = ['D0', 'C-', 'C0', 'C+', 'B-', 'B0', 'B+', 'A-', 'A0', 'A+']
+    print(f"#{test_case}", grade_list[the_grade])
+
+# print(cmp('output.txt', 'output2.txt'))
