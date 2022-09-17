f09ce770e9c933a3ac738566c041e7a341501741
dokkyunYU <ydgar001@naver.com>
2022-09-17 17:35:44 +0900

    Algorithm SWEA : 4008_숫자_만들기.py, 4013_특이한_자석.py

---
+++ Algorithm_Practice/self_practice/4008_숫자_만들기/4008_숫자_만들기_1.py
@@ -0,0 +1,13 @@
+# 2230 2340
+import sys
+
+sys.stdin = open("sample_input.txt", encoding="utf-8")
+
+for test_count in range(1, int(input()) + 1):
+    # 중복순열 문제...
+    numbers_count = int(input())
+    operator_count = list(map(int, input().split()))
+    the_numbers = list(map(int, input().split()))
+    print(the_numbers)
+    # 2 1 6 2
+    # 2 3 7 9 4 5 1 9 2 5 6 4
