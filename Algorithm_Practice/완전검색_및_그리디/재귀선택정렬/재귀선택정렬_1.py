32691ba63f4303dc079a97a3f8cc55d99bb1a308
dokkyunYU <ydgar001@naver.com>
2022-09-21 21:24:34 +0900

    Algorithm : 재귀선택정렬.py edited

--- Algorithm_Practice/완전검색_및_그리디/재귀선택정렬/재귀선택정렬_1.py
+++ Algorithm_Practice/완전검색_및_그리디/재귀선택정렬/재귀선택정렬_1.py
@@ -3,8 +3,10 @@ def recurs_select_sort(given_list, starting):
     if starting >= list_length:
         return
     min_idx = starting
+    min_value = given_list[starting]
     for i in range(starting + 1, list_length):
-        if given_list[i] < given_list[starting]:
+        if given_list[i] < min_value:
             min_idx = i
+            min_value = given_list[i]
     given_list[starting], given_list[min_idx] = given_list[min_idx], given_list[starting]
     recurs_select_sort(given_list, starting + 1)
