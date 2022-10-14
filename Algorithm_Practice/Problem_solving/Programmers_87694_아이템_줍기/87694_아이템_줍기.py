790eccfc99ab70d3abcc393549491549112912dc
dokkyunYU <ydgar001@naver.com>
2022-10-14 10:32:14 +0900

    아이템줍기

--- Algorithm_Practice/Problem_solving/Programmers_87694_아이템_줍기/87694_아이템_줍기.py
+++ Algorithm_Practice/Problem_solving/Programmers_87694_아이템_줍기/87694_아이템_줍기.py
@@ -11,7 +11,6 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
         for i in range(x1 + 1, x2):
             for j in range(y1 + 1, y2):
                 board[i][j] = 0
-    
     dist = 0
     item_dist = 0
     dx, dy = 0, 0
@@ -25,13 +24,23 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
         dy = 1
     else:
         dx = 1
-    # print(*board,sep="\n")
+    board[x][y] = 4
+    print(*board,sep="\n")
     # print(rectangle, characterX, characterY, itemX, itemY)
     # print(x,y,dx,dy, board[x+dx][y+dy])
     while True:
         # print(x,y,dx,dy, board[x+dx][y+dy])
+        x += dx
+        y += dy
+        dist += 1
+        if x == itemX and y == itemY:
+            item_dist = dist
+        if x == characterX and y == characterY:
+            return item_dist if item_dist < dist - item_dist else dist - item_dist
+        # print(x,y,dx,dy, board[x+dx][y+dy])
         while board[x][y] != 3:
-            print(x,y)
+            # print("????????????????", x,y, board[x][y])
+            board[x][y] = -board[x][y]
             x += dx
             y += dy
             dist += 1
@@ -39,33 +48,63 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
                 item_dist = dist
             if x == characterX and y == characterY:
                 return item_dist if item_dist < dist - item_dist else dist - item_dist
-        print(x,y,dx,dy, board[x+dx][y+dy])
+        # print(x,y,dx,dy, board[x+dx][y+dy])
         if dx:
-            if board[x][y+1] == 2 or board[x][y+1] == 3:
+            if board[x][y+1] == 4:
+                if board[x][y-1] > 0:
+                    dy = -1
+                    dx = 0
+                else:
+                    dist += 1
+                    return item_dist if item_dist < dist - item_dist else dist - item_dist 
+            elif board[x][y-1] == 4:
+                if board[x][y+1] > 0:
+                    dy = 1
+                    dx = 0
+                else:
+                    dist += 1
+                    return item_dist if item_dist < dist - item_dist else dist - item_dist 
+            elif board[x][y+1] == 2 or board[x][y+1] == 3:
                 dy = 1
                 dx = 0
             elif board[x][y-1] == 2 or board[x][y-1] == 3:
                 dy = -1
                 dx = 0
         elif dy:
-            if board[x+1][y] == 1 or board[x+1][y] == 3:
+            if board[x+1][y] == 4:
+                if board[x-1][y] > 0:
+                    dy = 0
+                    dx = -1
+                else:
+                    dist += 1
+                    return item_dist if item_dist < dist - item_dist else dist - item_dist 
+            elif board[x-1][y] == 4:
+                if board[x+1][y] > 0:
+                    dy = 0
+                    dx = 1
+                else:
+                    dist += 1
+                    return item_dist if item_dist < dist - item_dist else dist - item_dist 
+            elif board[x+1][y] == 1 or board[x+1][y] == 3:
                 dx = 1
                 dy = 0
             elif board[x-1][y] == 1 or board[x-1][y] == 3:
                 dx = -1
                 dy = 0
-        x += dx
-        y += dy
-        dist += 1
+        board[x][y] = -board[x][y]
         if x == itemX and y == itemY:
                 item_dist = dist
+                # print("!!!!!!!!!!!!!!!!", item_dist)
         if x == characterX and y == characterY:
             return item_dist if item_dist < dist - item_dist else dist - item_dist
+        
+        
 
 given = [[[[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]], 1, 3, 7, 8],
 [[[1, 1, 8, 4], [2, 2, 4, 9], [3, 6, 9, 8], [6, 3, 7, 7]], 9, 7, 6, 1],
 [[[1, 1, 5, 7]], 1, 1, 4, 7],
 [[[2, 1, 7, 5], [6, 4, 10, 10]], 3, 1, 7, 10],
-[[[2, 2, 5, 5], [1, 3, 6, 4], [3, 1, 4, 6]], 1, 4, 6, 3]]
+[[[2, 2, 5, 5], [1, 3, 6, 4], [3, 1, 4, 6]], 1, 4, 6, 3],
+[[[2, 1, 3, 6], [4, 1, 5, 6], [1, 2, 6, 3], [1, 4, 6, 5]], 3, 2, 5, 4]]
 for h in range(5):
-    print(solution(*given[h]))
+    print(solution(*given[5]))
