8347ff8cacf354d9542979a83cc4053719ac7aef
dokkyunYU <ydgar001@naver.com>
2022-11-08 22:18:46 +0900

    Algorithm : 92342_양궁대회.py

--- Algorithm_Practice/Problem_solving/Programmers_92342_양궁대회/Programmers_92342_양궁대회.py
+++ Algorithm_Practice/Problem_solving/Programmers_92342_양궁대회/Programmers_92342_양궁대회.py
@@ -1,26 +1,57 @@
 # [Programmers] 92342. 양궁대회
 # https://school.programmers.co.kr/learn/courses/30/lessons/92342
 
-def shooting(bullseye, arrows, nowlocation=0, totalsum=0, resultlist=[]):
+
+def shooting(bullseye, arrows, rivalscore, nowlocation=0, totalsum=0, resultlist=[]):
     global maxsum, maxlist
     if arrows == 0 or nowlocation == 10:
-        if totalsum > maxsum:
-            maxsum = totalsum
-            maxlist = resultlist
+        if (totalsum - rivalscore) > maxsum:
+            maxlist = []
+            maxsum = totalsum - rivalscore
+            maxlist.append(resultlist + [arrows])
+        elif (totalsum - rivalscore) == maxsum:
+            maxlist.append(resultlist + [arrows])
         return
     if arrows > bullseye[nowlocation]:
-        shooting(bullseye, arrows - bullseye[nowlocation] - 1, nowlocation + 1, totalsum + 10 - nowlocation, resultlist + [bullseye[nowlocation] + 1])
-    shooting(bullseye, arrows, nowlocation + 1, totalsum, resultlist + [0])
+        if bullseye[nowlocation]:
+            shooting(
+                bullseye,
+                arrows - bullseye[nowlocation] - 1,
+                rivalscore - (10 - nowlocation),
+                nowlocation + 1,
+                totalsum + 10 - nowlocation,
+                resultlist + [bullseye[nowlocation] + 1],
+            )
+        else:
+            shooting(
+                bullseye,
+                arrows - bullseye[nowlocation] - 1,
+                rivalscore,
+                nowlocation + 1,
+                totalsum + 10 - nowlocation,
+                resultlist + [bullseye[nowlocation] + 1],
+            )
+    shooting(bullseye, arrows, rivalscore, nowlocation + 1, totalsum, resultlist + [0])
 
 
 def solution(n, info):
     global maxsum, maxlist
     maxsum = 0
+    maxlist = []
     infoscore = 0
     for i in range(11):
         if info[i]:
             infoscore += 10 - i
-    shooting(info, n)
-    print(maxsum, maxlist)
+    shooting(info, n, infoscore)
     answer = []
+    max_length = 0
+    for i in maxlist:
+        length = len(i)
+        if max_length != max(max_length, length):
+            max_length = length
+            answer = i
+    if len(answer) < 11:
+        answer += [0] * (11 - len(answer))
+    if maxsum == 0:
+        return [-1]
     return answer
