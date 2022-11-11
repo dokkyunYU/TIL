14501d6a28ccea5566416c5798d11e6c75deadbc
dokkyunYU <ydgar001@naver.com>
2022-11-11 13:49:47 +0900

    Algorithm : BOJ_16235_나무재태크.py

---
+++ Algorithm_Practice/Problem_solving/BOJ_16235_나무재태크/16235_나무재태크_1.py
@@ -0,0 +1,6 @@
+# 나무 재태크
+# https://www.acmicpc.net/problem/16235
+
+n, m, k = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(n)]
+init = [list(map(int, input().split())) for _ in range(m)]
